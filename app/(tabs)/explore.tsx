d99e3b34913c7ff42b1fa11840a79489acc62991
Daniel Zhang <d.zhang200788@gmail.com>
2024-08-03 11:13:49 -0400

    sadkfsdklfj

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,124 +1,125 @@
 import Ionicons from "@expo/vector-icons/Ionicons";
-import { Switch, StyleSheet, Image, Platform } from "react-native";
-
+import { View, Switch, StyleSheet, Image, Platform, Text } from "react-native";
+import { useState } from "react";
 import { Collapsible } from "@/components/Collapsible";
 import { ExternalLink } from "@/components/ExternalLink";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 import { LinearGradient } from "expo-linear-gradient";
-
+import Slider from "@react-native-community/slider";
 function SettingWidget() {
     return (
-        <ThemedView>
-            <ThemedText type="title"></ThemedText>
-        </ThemedView>
+        <View style={styles.titleContainer}>
+            <Text style={styles.titleText}>Settings</Text>
+        </View>
     );
 }
-
-export default function TabTwoScreen() {
+function SliderWidget({ setting }: { setting: string }) {
+    const [range, useRange] = useState(50);
     return (
-        <ParallaxScrollView
-            headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
-            headerImage={<Ionicons size={310} name="code-slash" style={styles.headerImage} />}
-        >
-            <ThemedView style={styles.titleContainer}>
-                <ThemedText type="title">Explore</ThemedText>
-            </ThemedView>
-            <ThemedText>This app includes example code to help you get started.</ThemedText>
-            <Collapsible title="File-based routing">
-                <ThemedText>
-                    This app has two screens:{" "}
-                    <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> and{" "}
-                    <ThemedText type="defaultSemiBold">app/(tabs)/explore.tsx</ThemedText>
-                </ThemedText>
-                <ThemedText>
-                    The layout file in{" "}
-                    <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText> sets up
-                    the tab navigator.
-                </ThemedText>
-                <ExternalLink href="https://docs.expo.dev/router/introduction">
-                    <ThemedText type="link">Learn more</ThemedText>
-                </ExternalLink>
-            </Collapsible>
-            <Collapsible title="Android, iOS, and web support">
-                <ThemedText>
-                    You can open this project on Android, iOS, and the web. To open the web version,
-                    press <ThemedText type="defaultSemiBold">w</ThemedText> in the terminal running
-                    this project.
-                </ThemedText>
-            </Collapsible>
-            <Collapsible title="Images">
-                <ThemedText>
-                    For static images, you can use the{" "}
-                    <ThemedText type="defaultSemiBold">@2x</ThemedText> and{" "}
-                    <ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to provide files
-                    for different screen densities
-                </ThemedText>
-                <Image
-                    source={require("@/assets/images/react-logo.png")}
-                    style={{ alignSelf: "center" }}
-                />
-                <ExternalLink href="https://reactnative.dev/docs/images">
-                    <ThemedText type="link">Learn more</ThemedText>
-                </ExternalLink>
-            </Collapsible>
-            <Collapsible title="Custom fonts">
-                <ThemedText>
-                    Open <ThemedText type="defaultSemiBold">app/_layout.tsx</ThemedText> to see how
-                    to load{" "}
-                    <ThemedText style={{ fontFamily: "SpaceMono" }}>
-                        custom fonts such as this one.
-                    </ThemedText>
-                </ThemedText>
-                <ExternalLink href="https://docs.expo.dev/versions/latest/sdk/font">
-                    <ThemedText type="link">Learn more</ThemedText>
-                </ExternalLink>
-            </Collapsible>
-            <Collapsible title="Light and dark mode components">
-                <ThemedText>
-                    This template has light and dark mode support. The{" "}
-                    <ThemedText type="defaultSemiBold">useColorScheme()</ThemedText> hook lets you
-                    inspect what the user's current color scheme is, and so you can adjust UI colors
-                    accordingly.
-                </ThemedText>
-                <ExternalLink href="https://docs.expo.dev/develop/user-interface/color-themes/">
-                    <ThemedText type="link">Learn more</ThemedText>
-                </ExternalLink>
-            </Collapsible>
-            <Collapsible title="Animations">
-                <ThemedText>
-                    This template includes an example of an animated component. The{" "}
-                    <ThemedText type="defaultSemiBold">components/HelloWave.tsx</ThemedText>{" "}
-                    component uses the powerful{" "}
-                    <ThemedText type="defaultSemiBold">react-native-reanimated</ThemedText> library
-                    to create a waving hand animation.
-                </ThemedText>
-                {Platform.select({
-                    ios: (
-                        <ThemedText>
-                            The{" "}
-                            <ThemedText type="defaultSemiBold">
-                                components/ParallaxScrollView.tsx
-                            </ThemedText>{" "}
-                            component provides a parallax effect for the header image.
-                        </ThemedText>
-                    ),
-                })}
-            </Collapsible>
-        </ParallaxScrollView>
+        <View style={styles.containerSettings}>
+            <Text style={{ color: "white", fontSize: 20 }}>{setting}</Text>
+            <Text style={{ color: "white", fontSize: 20 }}>{range}</Text>
+            <Slider
+                style={{ width: 200, height: 40, padding: 10 }}
+                minimumValue={0}
+                maximumValue={100}
+                minimumTrackTintColor="#FFFFFF"
+                maximumTrackTintColor="#000000"
+                thumbTintColor="#FFFFFF"
+                value={range}
+                onValueChange={useRange}
+            />
+        </View>
     );
 }
+function SwitchWidget({ setting }: { setting: string }) {
+    const [isEnabled, setIsEnabled] = useState(false);
+    function toggleSwitch() {
+        setIsEnabled(!isEnabled);
+    }
 
+    return (
+        <View style={styles.containerSettings}>
+            <Text style={{ color: "white", fontSize: 20 }}>{setting}</Text>
+            <Switch
+                trackColor={{ false: "#767577", true: "#81b0ff" }}
+                thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+                ios_backgroundColor="#3e3e3e"
+                onValueChange={toggleSwitch}
+                value={isEnabled}
+            />
+        </View>
+    );
+}
+export default function App() {
+    return (
+        <View style={styles.container}>
+            <LinearGradient
+                colors={["#707071", "black"]}
+                start={{ x: 0, y: 0 }}
+                end={{ x: 0, y: 1 }}
+                style={styles.box}
+            />
+            <SettingWidget />
+            <SliderWidget setting="Pain" />
+            <SliderWidget setting="Audio" />
+            <SwitchWidget setting="Dark Mode" />
+        </View>
+    );
+}
 const styles = StyleSheet.create({
+    containerSettings: {
+        flex: 1,
+
+        // justifyContent: 'center',
+        alignItems: "center",
+        width: "100%",
+        height: "50%",
+        backgroundColor: "gray",
+    },
     headerImage: {
         color: "#808080",
         bottom: -90,
         left: -35,
         position: "absolute",
     },
+    // titleContainer: {
+    //     flexDirection: "row",
+    //     gap: 8,
+    // },
+    container: {
+        flex: 1,
+
+        // justifyContent: 'center',
+        alignItems: "center",
+        // Optionally, add padding or margins here
+    },
+    box: {
+        ...StyleSheet.absoluteFillObject,
+        // Or use flex to cover the whole screen
+        //  flex: 1,
+    },
     titleContainer: {
-        flexDirection: "row",
-        gap: 8,
+        margin: 20,
+        // position: 'absolute',
+        alignItems: "center",
+        // backgroundColor: '#2e303d',
+        borderRadius: 10,
+    },
+    button: {
+        paddingVertical: 20,
+        paddingHorizontal: 40,
+    },
+    titleText: {
+        color: "#fff",
+        textAlign: "center",
+        fontSize: 24,
+    },
+    touchable: {
+        // Ensure touchable opacity fills gradient
+        flex: 1,
+        justifyContent: "center",
     },
 });
--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -3,27 +3,17 @@
 SoftwareSerial sSerial(2, 3);
 
 void setup() {
-  Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for Native USB only
-  }
-
-  Serial.println("Goodnight moon!");
+  //   Serial.begin(9600);
+  //   while (!Serial) {
+  //     ; // wait for serial port to connect. Needed for Native USB only
+  //   }
 
   // set the data rate for the SoftwareSerial port
   sSerial.begin(9600);
-  sSerial.println("Hello, world?");
+  //   sSerial.println("Hello, world?");
 }
 
 void loop() {
-  if (sSerial.available()) {
-    int msg = sSerial.read();
-    Serial.write(msg);
-    sSerial.write(msg);
-  }
-  if (Serial.available()) {
-    int msg = Serial.read();
-    Serial.write(msg);
-    sSerial.write(msg);
-  }
+  int level = analogRead(0);
+  sSerial.print(String(level, HEX) + ";");
 }
